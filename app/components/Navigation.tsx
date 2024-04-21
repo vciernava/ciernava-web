51c8d9afcd9c6b5fb7a1d70666d8901d65bfb663
Viktor Čierňava <kontakt@viktorciernava.eu>
2024-04-21 22:28:31 +0200

    making button padding in navigation to forcebly overwrite the default padding.

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -19,7 +19,7 @@ const Navigation = () => {
                 <NavLink href="#experience">Experience</NavLink>
                 <NavLink href="#projects">Projects</NavLink>
                 <NavLink href="#contact">Contact</NavLink>
-                <Button href='/' className="ml-2 text-sm px-4 py-2">Resume</Button>
+                <Button href='/' className="ml-2 text-sm !px-4 !py-2">Resume</Button>
               </ol>
             </div>
           </nav>
