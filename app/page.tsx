f6fc7a3508b427bd704b12c83c0d14a245001e75
Viktor Čierňava <kontakt@viktorciernava.eu>
2024-08-31 11:51:47 +0200

    adding buy me a coffee, fixing tabs miscalculations

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,12 @@ import { TabsData } from './data/tabsData';
 import { ProjectsData } from './data/projectsData';
 import ProjectCard from './components/ProjectCard';
 import IconsBanner from './assets/icons-banner';
+import Script from 'next/script';
 
 export default function Home() {
   return (
     <main className='min-h-screen container max-w-1600'>
+      <script data-name="BMC-Widget" data-cfasync="true" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="vciernava" data-description="Support me on Buy me a coffee!" data-message="If you wish to support my work, you can buy me a coffee" data-color="#818cf8" data-position="Right" data-x_margin="18" data-y_margin="18" defer />
       <section className='section-hero' id='hero'>
         <h1>Hi, my name is</h1>
         <h2>Viktor Čierňava. ✌️</h2>
@@ -25,7 +27,7 @@ export default function Home() {
           focused on building accessible products for business owners.
         </p>
         <Button href='https://github.com/vciernava'>Checkout my GitHub!</Button>
-        <IconsBanner className='absolute bottom-0 left-1/2 -translate-x-1/2 scale-[.65]' />
+        <IconsBanner className='hidden md:block absolute bottom-0 left-1/2 -translate-x-1/2 scale-[.65]' />
       </section>
       <section className='section section-col-2' id='about'>
         <CountingHeading>About me</CountingHeading>
@@ -89,7 +91,7 @@ export default function Home() {
       <section className='section' id='projects'>
         <CountingHeading>Some Things I&apos;ve Build</CountingHeading>
         <div className='inner'>
-          <div className='projects grid gap-3 grid-cols-3'>
+          <div className='projects grid gap-3 grid-cols-2 md:grid-cols-3'>
             {ProjectsData.map((project) => (
               !project.hidden &&
               <ProjectCard
